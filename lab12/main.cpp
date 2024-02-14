b1e2b78112e909c8815eb6f625fe9d415c791d56
Kseniia Prokopovich <prokopovichksenia@gmail.com>
2024-02-14 09:58:48 +0300

    second

--- lab12/main.cpp
+++ lab12/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <list>
-#include <vector>
-#include <deque>
 using namespace std;
+
 template<typename T>
 class RangeIter {
 private:
+    // typename нужно, так как итератор зависит от шаблона T - контейнера. компилятор не может проверять T (на данный момент у него нет определения), и поэтому он не знает, является ли T::iterator статическим полем или типом. в такой ситуации компилятор предполагает, что это поле, поэтому это приводит к синтаксической ошибке. чтобы решить проблему, просто сообщите компилятору, что это тип, указав имя типа перед объявлением
     typename T::iterator current;
     typename T::iterator end;
 public:
@@ -20,7 +20,8 @@ public:
         --current;
         return *this;
     }
-
+    
+    // T::reference указывает на тип ссылки на элемент контейнера, которая будет возвращена оператором разыменования
     typename T::reference operator*() {
         return *current;
     }
@@ -32,6 +33,11 @@ public:
     bool operator!=(const RangeIter& other) {
         return current != other.current;
     }
+    void print() {
+        for (typename T::iterator it = current; it != end; ++it) {
+            cout << *it << " ";
+        }
+    }
 };
 
 template<typename T>
@@ -40,36 +46,28 @@ private:
     T& container;
 public:
     Range(T& container) : container(container) {}
-
-    RangeIter<T> begin(int n) {
-        typename T::iterator temp = container.begin();
+    RangeIter<T> setRange(int n, long long k) {
+        typename T::iterator temp1 = container.begin();
         while (n != 0) {
-            ++temp;
+            ++temp1;
             n--;
         }
-        return RangeIter<T>(temp, container.end());
-    }
-
-    RangeIter<T> end(long long n) {
-        typename T::iterator temp = container.end();
-        long long k = size(container) - n - 1;
-        while (k != 0) {
-            --temp;
-            k--;
+        typename T::iterator temp2 = container.end();
+        long long h = size(container) - k - 1;
+        while (h != 0) {
+            --temp2;
+            h--;
         }
-        return RangeIter<T>(temp, temp);
+        return RangeIter<T>(temp1, temp2);
     }
 };
 
 int main() {
-    list<int> l = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    Range<list<int>> range(l);
-
-    cout << "Iterating over list: ";
-    for (RangeIter it = range.begin(3); it != range.end(7); ++it) {
-        cout << *it << " ";
-    }
+    list<int> myList = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    Range<list<int>> range(myList);
+    RangeIter it = range.setRange(3, 7);
+    cout << "Элементы списка из заданного диапазона: ";
+    it.print();
     cout << endl;
-
     return 0;
 }
